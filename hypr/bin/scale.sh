7fadb3460ccae50fc42c2f90e091f90e8137be75
newtonhonk <nasendackel@posteo.de>
2024-02-25 10:58:23 +0100

    Add files via upload

--- hypr/bin/scale.sh
+++ hypr/bin/scale.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 monitor="eDP-1"
-resolution="2880x1800@120.00000"
-resolution96="2880x1800@96.01"
+resolution="2880x1800@120.000000"
 position="0x0"
 
 action(){
-    whattodo=$(echo -e "🔎 scale (1.0)\n🔎 scale (1.3)\n🔎 scale (1.8)\n🔎 scale (2.0)\n💡 96hz\n💡 120hz" | rofi -dmenu -p "🔴   Just what do you think you're scaling, Dave?")
+    whattodo=$(echo -e "🔎 scale (1.0)\n🔎 scale (1.3)\n🔎 scale (1.8)\n🔎 scale (2.0)" | rofi -dmenu -p "🔴   Just what do you think you're scaling, Dave?")
 	if [[ $whattodo == "🔎 scale (1.0)" ]]; then
         hyprctl keyword monitor "$monitor,$resolution,$position,1"
     elif [[ $whattodo == "🔎 scale (1.3)" ]]; then
@@ -14,8 +13,8 @@ action(){
 		hyprctl keyword monitor "$monitor,$resolution,$position,1.8"
 	elif [[ $whattodo == "🔎 scale (2.0)" ]]; then
 		hyprctl keyword monitor "$monitor,$resolution,$position,2"
-  	elif [[ $whattodo == "💡 96hz" ]]; then
-		hyprctl keyword monitor "$monitor,$resolution96,$position,2"
+  	elif [[ $whattodo == "💡 60hz" ]]; then
+		hyprctl keyword monitor "$monitor,$resolution,$position,2"
 	elif [[ $whattodo == "💡 120hz" ]]; then
 		hyprctl keyword monitor "$monitor,$resolution,$position,2"
     fi
